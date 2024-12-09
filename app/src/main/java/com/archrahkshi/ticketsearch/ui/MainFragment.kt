6b194d9e963d3e73822a13634d8490ffd8b14f88
smirnov <smirnov@rnd.stcnet.ru>
2024-12-10 00:52:03 +0300

    Save entered departure text to shared preferences, display on app restart

--- app/src/main/java/com/archrahkshi/ticketsearch/ui/MainFragment.kt
+++ app/src/main/java/com/archrahkshi/ticketsearch/ui/MainFragment.kt
@@ -1,15 +1,35 @@
 package com.archrahkshi.ticketsearch.ui
 
+import android.content.Context.MODE_PRIVATE
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.core.content.edit
+import androidx.fragment.app.Fragment
 import com.archrahkshi.ticketsearch.R
+
+private const val SAVED_DEPARTURE_TEXT = "SAVED_DEPARTURE_TEXT"
 
 class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sharedPreferences = requireActivity().getPreferences(MODE_PRIVATE)
+        with(requireActivity().findViewById<EditText>(R.id.departure_text_field)) {
+            setText(sharedPreferences.getString(SAVED_DEPARTURE_TEXT, ""))
+            setOnFocusChangeListener { _, hasFocus ->
+                if (!hasFocus) {
+                    sharedPreferences.edit {
+                        putString(SAVED_DEPARTURE_TEXT, text.toString())
+                    }
+                }
+            }
+        }
+    }
 }
