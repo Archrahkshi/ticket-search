4a8d438f6bbdc0f83cf772b9a8866d416f9974e4
smirnov <smirnov@rnd.stcnet.ru>
2024-12-12 21:02:15 +0300

    Optimize imports

--- app/src/main/java/com/archrahkshi/ticketsearch/ui/MainActivity.kt
+++ app/src/main/java/com/archrahkshi/ticketsearch/ui/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.archrahkshi.ticketsearch.ui
 
-import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.archrahkshi.ticketsearch.R
 import com.archrahkshi.ticketsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
