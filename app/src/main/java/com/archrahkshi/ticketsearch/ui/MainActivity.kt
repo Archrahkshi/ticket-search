3282dc14f2e0099e3ad421cbae588658f341e4f4
smirnov <smirnov@rnd.stcnet.ru>
2024-12-14 01:51:20 +0300

    Add NYI links to tabs

--- app/src/main/java/com/archrahkshi/ticketsearch/ui/MainActivity.kt
+++ app/src/main/java/com/archrahkshi/ticketsearch/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.archrahkshi.ticketsearch.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.archrahkshi.ticketsearch.R
 import com.archrahkshi.ticketsearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         views = ActivityMainBinding.inflate(layoutInflater)
         setContentView(views.root)
+
+        with(views.menuBar) {
+            listOf(hotelsTab, pinTab, subscriptionsTab, profileTab).forEach {
+                it.setOnClickListener {
+                    openNyiFragment()
+                }
+            }
+        }
+    }
+
+    private fun openNyiFragment() {
+        val fragment = supportFragmentManager.fragments.find { it is NyiFragment }
+        if (fragment == null || !fragment.isVisible) {
+            supportFragmentManager.commit {
+                add(R.id.fragment_container, NyiFragment())
+                addToBackStack(null)
+            }
+        }
     }
 }
