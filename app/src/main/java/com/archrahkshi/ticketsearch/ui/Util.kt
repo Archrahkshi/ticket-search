2b00fe761c245c780ab288876488e16611a8565f
smirnov <smirnov@rnd.stcnet.ru>
2024-12-14 01:38:29 +0300

    Prettify prices

--- app/src/main/java/com/archrahkshi/ticketsearch/ui/Util.kt
+++ app/src/main/java/com/archrahkshi/ticketsearch/ui/Util.kt
@@ -1,4 +1,16 @@
 package com.archrahkshi.ticketsearch.ui
 
+import android.content.Context
+import com.archrahkshi.ticketsearch.R
+import java.text.NumberFormat
+import java.util.Locale
+
 const val DEPARTURE_TEXT_KEY = "SAVED_DEPARTURE_TEXT_KEY"
 const val DESTINATION_TEXT_KEY = "SAVED_DESTINATION_TEXT_KEY"
+
+fun applyPriceTemplate(context: Context, price: Int) = context.getString(
+    R.string.price_template,
+    NumberFormat.getInstance(Locale("ru")).apply {
+        isGroupingUsed = true
+    }.format(price).replace(' ', '\u00A0')
+)
